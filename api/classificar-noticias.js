fb0268d2a8c099ac500df9cd119c55793cf6468d
Marcelo L Salvestro <salvestromarcelo@gmail.com>
2026-07-16 10:47:09 -0300

    Update classificar-noticias.js

--- api/classificar-noticias.js
+++ api/classificar-noticias.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
                 "Authorization": `Bearer ${process.env.NVIDIA_API_KEY}`
             },
             body: JSON.stringify({
-                model: "google/gemma-3-4b-it",
+                model: "google/gemma-3n-e4b-it",
                 messages: [{ role: "user", content: prompt }],
                 temperature: 0.1,
                 max_tokens: 2048
